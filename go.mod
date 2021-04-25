aef5b9b2167cd10cdc141e21f78e785da7ca8088
machao <machao@babeltime.com>
2021-04-25 17:04:59 +0800

    fix bug

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/go-redis/redis/v8 v8.8.2
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/panjf2000/ants/v2 v2.4.4 // indirect
 	gitlab.babeltime.com/packagist/blogger v0.0.0-20210423070342-4fb8ca8a37de
 )
--- test/addTestDatat.go
+++ test/addTestDatat.go
@@ -7,7 +7,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/panjf2000/ants/v2"
 	"math/rand"
+	"runtime"
 	"sync"
 	"time"
 	"upload_fcm_data/global"
@@ -27,58 +29,17 @@ func main() {
 	initialize.InitConfig()
 	var wg sync.WaitGroup
 	fmt.Println("start add data")
-
-	listKey := global.GetRdbKey(gn, bizid)
-	piArr := [...]string{
-		"1fffbjzos82bs9cnyj1dna7d6d29zg4esnh99u",
-		"1fffbkmd9ebtwi7u7f4oswm9li6twjydqs7qjv",
-		"1fffblf892i0p1zh6wlec2quukxtw29v4yismp",
-		"1fffbmr55j92gttv5wxspm0mgvw8x3p0n7cy0j",
-		"1fffbjqfba5y6uwr55cdak6faokhm4s02qkyue",
-		"1fffbkrwndszes1sngfx3v6pdqh87fi4zhz9ur",
-		"1fffbl6st3fbp199i8zh5ggcp84fgo3rj7pn1y",
-		"1fffbmzwmr1k3y8bri2linqbhnvmu510u5jj6z",
+	defer global.Rdb.Close()
+	p, _ := ants.NewPoolWithFunc(1000, func(i interface{}) {
+		pushTestData()
+		wg.Done()
+	})
+	defer p.Release()
+	// Submit tasks one by one.
+	for i := 0; i < randNum; i++ {
+		wg.Add(1)
+		_ = p.Invoke(int32(i))
 	}
-
-	ctArr := [...]int{
-		0,
-		2,
-	}
-	wg.Add(randNum)
-	// 新增上报数据
-	for i := 1; i <= randNum; i++ {
-		go func(i int, wg *sync.WaitGroup) {
-			pi := piArr[i%8]
-			ct := ctArr[getRand(0, 2)]
-			if 2 == ct {
-				pi = ""
-			}
-			// 模拟数据
-			/****
-			 * si           游戏内部会话标识
-			 * bt           用户行为类型 0 1  0：下线\登出；1：上线\登入},
-			 * ot           行为发生时间
-			 * ct           {上报类型，0：已认证通过用户；2：游客用户},
-			 * di           设备标识
-			 * pi           用户唯一标识
-			 */
-			newData := map[string]interface{}{}
-			newData["si"] = getmd5(pi)
-			newData["bt"] = getRand(0, 2)
-			newData["ot"] = time.Now().Unix() - int64(getRand(0, 60))
-			newData["ct"] = ct
-			newData["di"] = getmd5(pi)
-			newData["pi"] = pi
-			newStr, _ := json.Marshal(newData)
-			ctx := context.Background()
-			_, err := global.Rdb.LPush(ctx, listKey, string(newStr)).Result()
-			if err != nil {
-				panic("add data err")
-			}
-			wg.Done()
-		}(i, &wg)
-	}
-
 	wg.Wait()
 	fmt.Println("done")
 }
@@ -95,4 +56,51 @@ func getRand(start, end int) (r int) {
 	t := end - start
 	r = rand.Intn(t) + start
 	return
+}
+
+func pushTestData() {
+	listKey := global.GetRdbKey(gn, bizid)
+	piArr := [...]string{
+		"1fffbjzos82bs9cnyj1dna7d6d29zg4esnh99u",
+		"1fffbkmd9ebtwi7u7f4oswm9li6twjydqs7qjv",
+		"1fffblf892i0p1zh6wlec2quukxtw29v4yismp",
+		"1fffbmr55j92gttv5wxspm0mgvw8x3p0n7cy0j",
+		"1fffbjqfba5y6uwr55cdak6faokhm4s02qkyue",
+		"1fffbkrwndszes1sngfx3v6pdqh87fi4zhz9ur",
+		"1fffbl6st3fbp199i8zh5ggcp84fgo3rj7pn1y",
+		"1fffbmzwmr1k3y8bri2linqbhnvmu510u5jj6z",
+	}
+
+	ctArr := [...]int{
+		0,
+		2,
+	}
+	pi := piArr[rand.Intn(8)]
+	ct := ctArr[getRand(0, 2)]
+	if 2 == ct {
+		pi = ""
+	}
+	// 模拟数据
+	/****
+	 * si           游戏内部会话标识
+	 * bt           用户行为类型 0 1  0：下线\登出；1：上线\登入},
+	 * ot           行为发生时间
+	 * ct           {上报类型，0：已认证通过用户；2：游客用户},
+	 * di           设备标识
+	 * pi           用户唯一标识
+	 */
+	newData := map[string]interface{}{}
+	newData["si"] = getmd5(pi)
+	newData["bt"] = getRand(0, 2)
+	newData["ot"] = time.Now().Unix() - int64(getRand(0, 60))
+	newData["ct"] = ct
+	newData["di"] = getmd5(pi)
+	newData["pi"] = pi
+	newStr, _ := json.Marshal(newData)
+	ctx := context.Background()
+	_, err := global.Rdb.LPush(ctx, listKey, string(newStr)).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(runtime.NumGoroutine())
 }
